a59cafe4b51badbdeb0d044c52bf47c07d7d81d4
kaysvoboda <kaymcleay@gmail.com>
2021-02-22 19:38:34 -0500

    kaylie test branch

---
+++ MidtermProject_POSApplication/MidtermProject_POSApplication/KStestclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidtermProject_POSApplication
+{
+    class KStestclass
+    {
+        public static string TestClass()
+        {
+
+
+            string test = "This is a test edit";
+            return test;
+
+        }
+    }
+
+}
