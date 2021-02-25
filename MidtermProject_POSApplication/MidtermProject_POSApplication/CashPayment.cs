78b4ac16ee622276bc96790605f9f6d2254298ac
kaysvoboda <kaymcleay@gmail.com>
2021-02-24 21:36:26 -0500

    new commit

--- MidtermProject_POSApplication/MidtermProject_POSApplication/CashPayment.cs
+++ MidtermProject_POSApplication/MidtermProject_POSApplication/CashPayment.cs
@@ -7,12 +7,24 @@ namespace MidtermProject_POSApplication
     public class CashPayment : IPayment
     {
         public double AmountTendered { get; set; }
+        public double ChangeOwed { get; set; }
+
+        public double GetCashPayment()
+        {
+            Console.Write("Amount tendered: ");
+            string tendered = Console.ReadLine();
+            double amountTendered = Double.Parse(tendered);
+            AmountTendered = amountTendered;
+            return AmountTendered;
+
+        }
 
         public double ProvideChange(double amountTendered, double total) 
         {
-            AmountTendered = amountTendered;
-            double change = amountTendered - total;
-            return change;
+            //AmountTendered = amountTendered;
+            double changeOwed = amountTendered - total;
+            ChangeOwed = changeOwed;
+            return ChangeOwed;
         }
 
     }
