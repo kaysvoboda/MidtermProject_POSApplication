3906a22d81f16963f852b74aea4b8790d742e8f4
kaysvoboda <kaymcleay@gmail.com>
2021-03-03 22:00:59 -0500

    last commit 3/3

---
+++ MidtermProject_POSApplication/MidtermProject_POSApplication/HistoricalOrders.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidtermProject_POSApplication
+{
+    public class HistoricalOrders
+    {
+        public int OrderNumber { get; set; }
+
+        public int UserSelection { get; set; }
+        public int UserQuantity { get; set; }
+        public string ItemName { get; set; }
+
+
+        public HistoricalOrders(int orderNumber, string itemName, int userSelection, int userQuantity)
+        {
+            ItemName = itemName;
+            OrderNumber = orderNumber;
+            UserSelection = userSelection;
+            UserQuantity = userQuantity;
+        }
+
+
+
+    }
+}
